278a74f55d679abcc2267f81650cdd7207938b0d
AndVaz2001 <andresvazquez2001@gmail.com>
2025-08-11 10:28:42 +0200

    Minor adjustments

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,3 +1,27 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export function Home() {
-  return <div>Home Page</div>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>ROI_Formula </CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  )
 }
